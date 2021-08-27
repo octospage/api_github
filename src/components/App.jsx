5f550fd1e4cda187ea91393843be18df319e95cc
Tatyana Lebedeva <octanchik@gmail.com>
2021-08-27 22:55:35 +0300

    add babel

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'; //rsc
+import 'App.less';
+const App = () => {
+    return (
+        <div className="App">
+            It a ALIve
+        </div>
+    );
+};
+
+export default App;
