71ea92a64b3bb8093a24ec1132e41a96148225ec
Tatyana Lebedeva <octanchik@gmail.com>
2021-08-29 12:54:31 +0300

    add search

--- src/components/actions/repos.js
+++ src/components/actions/repos.js
@@ -1,8 +1,11 @@
 import axios from "axios";
-import {setRepos} from "../../reducers/reposReducer";
+import {setIsFetching, setRepos} from "../../reducers/reposReducer";
 
 export const getRepos = (searchQuery = "stars") => {
     return async (dispatch) => {
+        searchQuery = searchQuery.length ? searchQuery : "stars";
+
+        dispatch(setIsFetching(true))
         const url = `https://api.github.com/search/repositories?q=${searchQuery}&sort=stars`;
         const response = await axios.get(url);
         dispatch(setRepos(response.data))
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './main.less'
 import {useDispatch, useSelector} from "react-redux";
 import {getRepos} from "../actions/repos";
@@ -7,19 +7,39 @@ import Repo from "./repo/Repo";
 
 const Main = () => {
     const dispatch = useDispatch();
+    const repos = useSelector(state => state.repos.items);
+    const isFetching = useSelector(state => state.repos.isFetching);
+    const [searchValue, setSearchValue] = useState('')
 
-    const repos = useSelector(state => state.repos.items)
 
     useEffect(() => {
         dispatch(getRepos())
     }, [])
 
+    function searchHandler(){
+        dispatch(getRepos(searchValue))
+    }
+
     return (
         <div className="container">
-            {repos.map(repo =>
+            <div className="search">
+                <input className="search__input"
+                       type="text"
+                       value={searchValue}
+                       onChange={(e) => setSearchValue(e.target.value)}
+                />
+                <button className="search__button" onClick={() => searchHandler()}>Search</button>
+            </div>
+
+            { isFetching === false
+                ?
+                repos.map(repo =>
                 <Repo repo={repo}/>
-            )}
-            
+            )
+                :
+                <div className='fetching'></div>
+            }
+
         </div>
     );
 };
